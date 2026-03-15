469609f00b0f7e6720e3303f3d9f9834dea7190e
ananyasamal27 <ananyasamal2704@gmail.com>
2026-03-15 23:10:14 +0530

    fix database name to tradesphere

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://tradesphere-x.onrender.com/api';
 
 const api = axios.create({ baseURL: BASE_URL });
 
